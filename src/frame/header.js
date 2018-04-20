0cd52d43156cd96b00504e38a50e415498e6b0a1
odask <oda.skaug@hotmail.com>
2018-04-20 14:11:38 +0200

    Work in progress: header frame

--- src/frame/header.js
+++ src/frame/header.js
@@ -3,18 +3,24 @@ import ConnectionError from '../error';
 import Frame from './frame';
 
 export default class HeadersFrame extends Frame{
+  headers = {};
+  exclusive;
+  weight;
+  streamDependency;
 
-constructor(opts){
-  super(FrameTypes.HEADERS, opts);
-  if(this.flags.PADDED){
-    let lenghtPadding = this.payload.readUIntBE8(0);
-    if(Buffer.compare(new Buffer(lenghtPadding), this.payload.slice(this.payload.padding - lenghtPadding)) != 0)
-      return; //// TODO:
-    this.payload = this.payload.slice(1, this.payload.padding - lenghtPadding)
-    } else if (this.flags.PRIORITY) {
-
-    } else {
-
+  constructor(opts){
+    super(FrameTypes.HEADERS, opts);
+    if(this.flags.PADDED){
+      let lenghtPadding = this.payload.readUInt8(0);
+      if(Buffer.compare(new Buffer(lenghtPadding), this.payload.slice(this.payload.padding - lenghtPadding)) != 0)
+        return; //// TODO:
+      this.payload = this.payload.slice(1, this.payload.padding - lenghtPadding)
+    }
+    else if (this.flags.PRIORITY) {
+      this.exclusive = (this.payload.readUInt32BE(0)&(0x1 << 31))!= 0;
+      this.weight = this.payload.readUInt8(4);
+      this.streamDependency = (this.payload.readUInt32BE(0)^(0x1 << 31));
     }
   }
-}
+
+  
