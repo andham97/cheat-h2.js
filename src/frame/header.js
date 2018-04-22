48e91c0f7b98bb19f7fb919399e0eaf7b8df1804
odask <oda.skaug@hotmail.com>
2018-04-22 11:15:29 +0200

    Work in progress

--- src/frame/header.js
+++ src/frame/header.js
@@ -8,19 +8,26 @@ export default class HeadersFrame extends Frame{
   weight;
   streamDependency;
 
-  constructor(opts){
-    super(FrameTypes.HEADERS, opts);
+  constructor(opitions){
+    super(FrameTypes.HEADERS, opitions);
     if(this.flags.PADDED){
       let lenghtPadding = this.payload.readUInt8(0);
-      if(Buffer.compare(new Buffer(lenghtPadding), this.payload.slice(this.payload.padding - lenghtPadding)) != 0)
+      if(Buffer.compare(new Buffer(lenghtPadding), this.payload.slice(this.payload.padding - lenghtPadding)) != 0);
         return; //// TODO:
-      this.payload = this.payload.slice(1, this.payload.padding - lenghtPadding)
+      this.payload = this.payload.slice(1, this.payload.padding - lenghtPadding);
     }
-    else if (this.flags.PRIORITY) {
+    if (this.flags.PRIORITY) {
       this.exclusive = (this.payload.readUInt32BE(0)&(0x1 << 31))!= 0;
       this.weight = this.payload.readUInt8(4);
       this.streamDependency = (this.payload.readUInt32BE(0)^(0x1 << 31));
+      this.payload = this.payload.slice(5);
     }
+
+    opitions.context.decompress(this.payload);
+
   }
 
-  
+  get_payload(){
+
+  }
+}
