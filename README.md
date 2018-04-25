e69055aff3cf8183a58c4c56518339ac08f425c5
odask <oda.skaug@hotmail.com>
2018-04-25 15:22:00 +0200

    testing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cheat-h2.js
-#### _It so simple it feels like cheating_
+#### _"It so simple it feels like cheating"_
 [![Build Status](https://travis-ci.org/andham97/cheat-js.svg?branch=master)](https://travis-ci.org/andham97/cheat-js)
 [![codecov](https://codecov.io/gh/andham97/cheat-js/branch/master/graph/badge.svg)](https://codecov.io/gh/andham97/cheat-js)
 
@@ -30,9 +30,11 @@
 
 # Usage
 
+
 # Installation
 
 # Tests
+
 
 # API documentation
 ```javascript
--- test/hpack.js
+++ test/hpack.js
@@ -1,17 +1,12 @@
 import Frame from '../../src/frame/frame';
 import chai from 'chai';
-import encode
+import {hpack_methods} from '../src/hpack';
 
 describe('encoding header request', () => {
-  var header = [
-    [':method', 'GET'],
-    [':path', '/'],
-    [':authority', 'www.vg.no']
-  ];
-
-  var expect = new Buffer([
-
-  ]);
-
-  let encoding = hpac
+  it('should encode a http header ', () =>{
+    let num = 10;
+    let prefix = 5;
+    0x0a;
+    chai.expect(hpack_methods.encode_integer(num,prefix)).to.deep.equal(new Buffer([0x0a]));
+  })
 });
