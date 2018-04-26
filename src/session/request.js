6d7d9740b210dd68dc69632197ce897ed1421563
Andreas Hammer <andreas.hammer.97@protonmail.com>
2018-04-26 11:32:29 +0200

    basic server push implementation

--- src/session/request.js
+++ src/session/request.js
@@ -1,4 +1,4 @@
-import {ConnectionError} from '../error';
+import {ConnectionError, StreamError} from '../error';
 import {ErrorCodes} from '../constants';
 
 export default class Request {
@@ -11,19 +11,28 @@ export default class Request {
     this.headers = {...headers};
     this.raw_data = Buffer.concat([new Buffer(0), data]);
     if(this.headers[':method']){
-      if(this.headers[':method'] == 'GET')
-        this.query = new Buffer((this.headers[':path'].indexOf('?') == -1 ? 0 : this.headers[':path'].split('?')[1]));
-      else
-        this.body = Buffer.concat([new Buffer(0), data]);
-    }
-    let path_query = this.headers[':path'].split('?');
-    this.headers[':path'] = path_query[0];
-    if(path_query.length == 2){
-      this.query = {};
-      let pairs = path_query[1].split('&');
-      pairs.forEach(pair => {
-        this.query[pair[0]] = pair[1];
-      });
+      if(this.headers[':method'] == 'POST' && this.headers['content-length'] > 0){
+        if(this.headers['content-encoding'])
+          throw new StreamError(ErrorCodes.INTERNAL_ERROR, 'non-supported encoding type');
+        let body = decodeURIComponent(data.toString());
+        body = body.split('&');
+        this.body = {};
+        body.forEach(element => {
+          let pair = element.split('=');
+          this.body[pair[0]] = pair[1];
+        });
+      }
+      else if(this.headers[':method'] == 'GET' && this.headers[':path']){
+        let path_query = this.headers[':path'].split('?');
+        this.headers[':path'] = path_query[0];
+        if(path_query.length == 2){
+          this.query = {};
+          let pairs = path_query[1].split('&');
+          pairs.forEach(pair => {
+            this.query[pair[0]] = pair[1];
+          });
+        }
+      }
     }
   }
 }
--- src/session/response.js
+++ src/session/response.js
@@ -4,6 +4,7 @@ export default class Response {
   headers;
   payload;
   is_sent = false;
+  required_paths = [];
 
   constructor(headers){
     this.headers = {
@@ -19,11 +20,24 @@ export default class Response {
 
   send(data){
     if(this.is_sent)
-      return; // Is sent error
+      throw new Error('Data is already sent');
     this.is_sent = true;
     if(data instanceof Buffer)
       this.payload = data;
-    else
+    else if(data == undefined)
+      this.payload = new Buffer(0);
+    else if(data.toString)
       this.payload = new Buffer(data.toString(), (this.headers['content-type'] && this.headers['content-type'].indexOf('charset') == -1 ? 'utf-8' : this.headers['content-type'].split('charset')[1].split(';')[0].split(' ')[0].split('=')[1]));
+  }
+
+  push(path){
+    if(!path)
+      return;
+    if(!path.method)
+      return;
+    if(!path.path)
+      return;
+    else
+      this.required_paths.push(path);
   }
 }
