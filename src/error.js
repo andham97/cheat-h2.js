cd48181e56f59be7470c407aab89e4f67bb64bfe
odask <oda.skaug@hotmail.com>
2018-04-26 19:46:31 +0200

    Testing: error.js

--- src/error.js
+++ src/error.js
@@ -19,9 +19,20 @@ export class StreamError extends Error {
   type = 0x2;
 
   constructor(error, msg, stream_id){
+    console.log('1')
     super(ErrorCodes.keys[error] + (typeof msg != 'undefined' ? ': ' + msg : ''));
+    console.log('2')
     Error.captureStackTrace(this, StreamError);
+    console.log('3')
     this.error_code = error;
+    console.log('4')
     this.stream_id = stream_id || -1;
   }
 }
+/*
+let error = ErrorCodes.INTERNAL_ERROR;
+let msg = "invalid";
+let stream_id = 1;
+let new_stream_error = new StreamError(error, msg, stream_id)
+console.log(new_stream_error)
+*/
---
+++ test/error.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+import {ErrorCodes} from '../src/constants';
+import {StreamError} from '../src/error';
+
+describe('Error class testing', () => {
+  it('Testing constructor of Stream errors', () => {
+    let error = ErrorCodes.INTERNAL_ERROR;
+    let msg = "invalid";
+    let stream_id = 1;
+    let new_stream_error = new StreamError(error, msg, stream_id);
+    chai.expect(new_stream_error.stream_id).to.equal(stream_id);
+  })
+})
